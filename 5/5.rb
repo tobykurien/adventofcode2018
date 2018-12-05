af1fb1db02a6e9a734fa3426d106899e3da555db
Toby Kurien <toby@tobykurien.com>
2018-12-05 20:46:04 +0200

    day 5 part 1

--- 5/5.rb
+++ 5/5.rb
@@ -1,34 +1,24 @@
-f = File.open('test.txt')
+f = File.open('data.txt')
 polymer = ""
-f.each_char{|c| polymer += c}
-polymer.strip!
-puts polymer.length
-
-def runRound(p)
-	a = p[0]
-	ret = ""
-	p[1..-1].each_char do |c|
-		diff = a.bytes[0] - c.bytes[0]
-		if diff == 32 or diff == -32
+last_c = 0
+f.each_char do |c|
+	cur_c = c.bytes[0]
+  if cur_c >= 65 and cur_c <= 122
+		if cur_c - last_c == 32 or cur_c - last_c == -32
 			# skip
-			a = '1'
+			polymer.chop!
+			if polymer.length > 0
+				last_c = polymer.bytes.last
+			else
+				last_c = 0
+			end
 		else
-			ret += a if a != '1'
-			a = c
+			polymer += c
+			last_c = cur_c
 		end
 	end
-	ret += a if a != '1'
-	return ret
 end
 
-len = polymer.length
-p = runRound(polymer)
-while (len != p.length)
-	len = p.length
-	p = runRound(p)
-end
-
-#puts p
-puts p.length 
-
+#puts polymer
+puts polymer.length
 
